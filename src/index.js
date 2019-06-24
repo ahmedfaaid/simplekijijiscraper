14f123d824d54b2123f719cf6ba07422a95e75c9
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2019-06-24 16:11:37 -0400

    Add client template

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const scraper = require("./scraper");
+const express = require('express');
+const scraper = require('./scraper');
 const app = express();
 
-app.get("/", (req, res) => {
-  scraper.searchMonitors().then(monitors => {
-    res.json(monitors);
-  });
+app.get('/', (req, res) => {
+	scraper.searchMonitors().then((monitors) => {
+		res.json(monitors);
+	});
 });
 
-app.get("/monitor/:id", (req, res) => {
-  scraper.getMonitor(req.params.id).then(monitor => {
-    res.json(monitor);
-  });
+app.get('/monitor/:id', (req, res) => {
+	scraper.getMonitor(req.params.id).then((monitor) => {
+		res.json(monitor);
+	});
 });
 
 app.listen(8080, () => {
-  console.log("We Outchea");
+	console.log('We Outchea');
 });
